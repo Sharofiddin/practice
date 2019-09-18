23eb292edf7af2bd3b1738c7b5694f8abdee2e06
sharofiddin <pardayev.sharofiddin@gmail.com>
2019-09-18 19:02:57 +0500

    road_bath

---
+++ CPP_Principles_and_Practice/part_01/Exercises/road_to_bathroom.cc
@@ -0,0 +1,18 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+	cout << "Face to south if you are not\n"
+		"walk until reach to stairs\n"
+		"up to 5 stairs\n"
+		"face to north\n"
+		"up to 5 stairs\n"
+		"walk 20 steps\n"
+		"turn left\n"
+		"if door is not locked, open door\n"
+		"else wait until door unlocked\n"
+		"open door\n"
+		"enter room\n";
+	return 0;
+}
+		

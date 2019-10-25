8e6904a87c8b08ac3f6174d17566def1e7b077fb
sharofiddin <pardayev.sharofiddin@gmail.com>
2019-10-25 10:10:54 +0500

    4-1

---
+++ CPP_Principles_and_Practice/part03/drill4-1.cc
@@ -0,0 +1,17 @@
+#include "../std_lib_facilities.h" 
+
+int main()
+{
+	char c;
+	int a;
+	int b;
+	cin >> c;
+	while( c!='|'){
+		cin >> a;
+		cin >> b;
+		cout << a <<' ' << b<< '\n';
+	}
+
+	return 0;
+}
+

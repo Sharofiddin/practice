d99252942fa8e8c6f23e1e77ce4fff15a27c458f
sharofiddin <pardayev.sharofiddin@gmail.com>
2021-02-09 19:46:59 +0500

    11-7 started

---
+++ accelerated/chapter11/11-7/Student_info.h
@@ -0,0 +1,31 @@
+#ifndef GUARD_Student_info_11_7
+#define GUARD_Student_info_11_7
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include "../Vec.h"
+class Student_info {
+public:
+	Student_info();              // construct an empty `Student_info' object
+	Student_info(std::istream&); // construct one by reading a stream
+	std::string name() const { return n; }
+	bool valid() const { return !homework.empty(); }
+
+	// as defined in 9.2.1/157, and changed to read into `n' instead of `name'
+	std::istream& read(std::istream&);
+
+	double grade() const;    // as defined in 9.2.1/158
+private:
+	std::string n;
+	double midterm; 
+	double final;
+	Vec<double> homework;
+	double total;
+	std::istream& read_hw(std::istream& , std::vector<double>& );//9-7
+};
+
+bool compare(const Student_info&, const Student_info&);
+
+#endif
+

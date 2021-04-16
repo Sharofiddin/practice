73f3fc37061e0c588497609d270830597499909e
sharofiddin <pardayev.sharofiddin@gmail.com>
2021-04-16 10:28:13 +0500

    14-1

--- accelerated/chapter14/1/14-1.cc
+++ accelerated/chapter14/1/14-1.cc
@@ -1,12 +1,35 @@
 #include "../Ptr.h"
+#include <iomanip>
 #include "../Core.h"
 #include <iostream>
-
-bool compare(const Ptr<Core> &p1, const Ptr<Core> &p2){
+#include <vector>
+#include <algorithm>
+bool compare_ptr(Ptr<Core> &p1, Ptr<Core> &p2){
     return p1->name() > p2->name();
 }
 int main()
 {
- 
+ std::vector<Ptr<Core>> students;
+ Ptr<Core> student;
+ std::string::size_type max_len = 0;
+ char type;
+ while(std::cin >> type){
+     student = type == 'U'? new Core : new Grad;
+     student->read(std::cin);
+     max_len = std::max(student->name().size(), max_len);
+     students.push_back(student);
+ }
+
+ std::sort(students.begin(), students.end(), compare_ptr);
+ for(std::vector<Ptr<Core>>::const_iterator it= students.begin(); it != students.end(); it++){
+     std::cout << (*it)->name() << std::string(max_len + 1 - (*it)->name().size(), ' ') << "| ";
+     try{
+         std::streamsize stsize = std::cout.precision();
+         double final = (*it)->grade();
+         std::cout << std::setprecision(3) << final << std::setprecision(stsize) << std::endl;
+     } catch(const std::domain_error &de){
+         std::clog << de.what() << std::endl; 
+     }
+ }
  return 0;
 }
